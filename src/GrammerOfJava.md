2670ec68b957cb9a6813443912b5eb3e833720f0
BeomSeogKim <kbs4520@daum.net>
2022-10-31 11:56:08 +0900

    1차수강

---
+++ src/GrammerOfJava.md
@@ -0,0 +1,7 @@
+# Grammar Of Java
+
+## 1. 문자열
+- 문자를 대문자로 변환 : .toUpperCase()
+- 문자를 소문자로 변환 : .toLowerCase()
+- String(str)을 문자열로 변환 : str.toCharArray() 
+- 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.*;
+class Main {
+    public int solution(String str, char t) {
+        int answer = 0;
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+
     }
 }

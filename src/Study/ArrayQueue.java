3c97c714867adf1b31cda5e5316af1f58a0e4aee
BeomSeogKim <kbs4520@daum.net>
2022-12-13 18:15:28 +0900

    Assignment1

--- src/Study/ArrayQueue.java
+++ src/Study/ArrayQueue.java
@@ -1,2 +1,64 @@
-package Study;public class ArrayQueue {
+package Study;
+
+public class ArrayQueue {
+    int rear;
+    int[] queue;
+    int queueSize;
+
+    public ArrayQueue(int size) {
+        this.rear = 0;
+        this.queue = new int[size];
+        this.queueSize = size;
+    }
+
+    // Check Empty
+    public boolean isEmpty() {
+        return rear == 0;
+    }
+
+    // Check Full
+    public boolean isFull() {
+        return (rear == this.queueSize - 1);
+    }
+
+    public void enqueue(int number) {
+        if (isFull()) {
+            System.out.println("[Warning] Queue is Full");
+        } else {
+            queue[rear++] = number;
+            System.out.println("Inserted Number = " + number);
+        }
+    }
+
+    public void dequeue() {
+        if (isEmpty()) {
+            System.out.println("[Warning] Queue is Empty");
+        } else {
+            int[] tmpQueue = queue.clone();
+            System.out.println("Deleted Number = " + tmpQueue[0]);
+            for (int i = 0; i < queue.length - 1; i++) {
+                queue[i] = tmpQueue[i + 1];
+            }
+            rear--;
+        }
+    }
+
+    public void peek() {
+        System.out.println("Queue : " + queue[0]);
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(5);
+        arrayQueue.dequeue();
+        arrayQueue.enqueue(1);
+        arrayQueue.peek();
+        arrayQueue.enqueue(8);
+        arrayQueue.peek();
+        arrayQueue.dequeue();
+        arrayQueue.peek();
+        arrayQueue.dequeue();
+        arrayQueue.peek();
+        arrayQueue.dequeue();
+    }
+
 }
--- src/Study/ArrayStack.java
+++ src/Study/ArrayStack.java
@@ -1,11 +1,11 @@
 package Study;
 
-public class Stack {
+public class ArrayStack {
     int top;
     int size;
-    int [] stack;
+    int[] stack;
 
-    public Stack(int size) {
+    public ArrayStack(int size) {
         this.size = size;
         stack = new int[size];
         top = -1;
@@ -13,16 +13,28 @@ public class Stack {
 
     public void push(int item) {
         stack[++top] = item;
-        System.out.println(stack[top] + "push");
+        System.out.println(stack[top] + " push");
     }
 
     public void pop() {
         System.out.println(stack[top] + "pop");
         int pop = stack[top];
-        stack[pop--] = 0;
+        stack[top--] = 0;
     }
+
     public void peek() {
         System.out.println(stack[top] + "peek");
+    }
+
+    public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(5);
+        arrayStack.push(1);
+        arrayStack.push(5);
+        arrayStack.push(9);
+        arrayStack.peek();
+        arrayStack.pop();
+        arrayStack.peek();
+        arrayStack.pop();
     }
 }
 
